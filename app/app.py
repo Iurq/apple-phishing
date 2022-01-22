bb54fd2bfa03e1a835578620e9909a9bdd5f8569
Iurq <66850320+Iurq@users.noreply.github.com>
2022-01-22 14:52:02 +0000

    Removed Path Assignments and OS Import
    Debugging "OSError : Path Already Exists" 
    Attempt 1

--- app/app.py
+++ app/app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template
-import os
 
-template_dir = os.path.abspath('C:/Users/xpyth/WebstormProjects/untitled3/html')
-static_dir = os.path.abspath('C:/Users/xpyth/WebstormProjects/untitled3/static')
-app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
