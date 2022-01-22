fbe786d3c16f3b4687bb39992847dcece10431b9
Iurq <66850320+Iurq@users.noreply.github.com>
2022-01-22 15:01:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
-import os
 
-template_dir = os.path.abspath('C:/Users/xpyth/WebstormProjects/untitled3/html')
-static_dir = os.path.abspath('C:/Users/xpyth/WebstormProjects/untitled3/static')
-app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-app.run()
+if __name__ == '__main__' :
+    app.run()
